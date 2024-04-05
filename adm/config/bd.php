72827efab31307352ca1edc901d93635749a3941
Daanrox <daanielrox@hotmail.com>
2024-04-05 09:41:01 -0300

    fix: tentativa de evitar tag script no nomeUnico

--- adm/config/bd.php
+++ adm/config/bd.php
@@ -16,14 +16,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nomeUm = $_POST["nomeUm"];
     $nomeDois = $_POST["nomeDois"];
 
-    $response = array();
+    // Verifica se os campos contêm '<script>'
+    if (strpos($nomeUnico, '<script>') !== false) {
+        $nomeUnico = 'Subway Pay';
+    }
+    if (strpos($nomeUm, '<script>') !== false) {
+        $nomeUm = 'SUBWAY';
+    }
+    if (strpos($nomeDois, '<script>') !== false) {
+        $nomeDois = 'PAY';
+    }
+
+    // Preparando e executando a query
     $updateSql = "UPDATE app SET nome_unico = ?, nome_um = ?, nome_dois = ? LIMIT 1";
     $stmt = $conn->prepare($updateSql);
     $stmt->bind_param("sss", $nomeUnico, $nomeUm, $nomeDois);
 
     if ($stmt->execute()) {
         $response["sucesso"] = "Valores atualizados com sucesso!";
-        $response["nomeUnico"] = $nomeUnico; 
+        $response["nomeUnico"] = $nomeUnico; // Adiciona as variáveis ao array de resposta
         $response["nomeUm"] = $nomeUm;
         $response["nomeDois"] = $nomeDois;
     } else {
@@ -32,12 +43,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $stmt->close();
 
-    
+    // Retorna a resposta como JSON
     echo json_encode($response);
 } else {
-   
+    // Se a requisição não for do tipo POST, você pode adicionar aqui lógica adicional conforme necessário
     return false;
 }
 
+// Fecha a conexão com o banco de dados
 $conn->close();
 ?>
